0a526986af03988a5c1d91db419244ae73e5aeab
Jbridg12 <joshua.bridges4@gmail.com>
2022-10-27 21:36:58 +0100

    Work with Chapter
    - Test scene changes in Chapter 1
    - Load in animators

--- Assets/Game/Scripts/Chapter1/Chapter1.cs
+++ Assets/Game/Scripts/Chapter1/Chapter1.cs
@@ -9,14 +9,24 @@ public class Chapter1 : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject[] characters;
     [SerializeField] private AICharacterControl[] aiCharacterControl;
+    [SerializeField] private Animator[] animators;
+
+    private bool nextScene;
+    private Animator playerAnimator;
 
     private void Awake()
     {
         aiCharacterControl = new AICharacterControl[characters.Length];
+        animators = new Animator[characters.Length];
+
         for (int i = 0; i < characters.Length; i++)
         {
             aiCharacterControl[i] = characters[i].GetComponent<AICharacterControl>();
+            animators[i] = characters[i].GetComponent<Animator>();
         }
+
+        playerAnimator = player.GetComponent<Animator>();
+
     }
 
     // Start is called before the first frame update
@@ -28,12 +38,41 @@ public class Chapter1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        //switch(player.name)
+
+
+        nextScene = true;
+        foreach (AICharacterControl item in aiCharacterControl)
+        {
+            if(item.agent.velocity.magnitude == 0.0f && item.targetTransform == null) nextScene = false;
+        }
+        
+        
+        if(nextScene)
+        {
+            print("Scene Change");
+            playerAnimator.SetTrigger("DrawGun");
+            playerAnimator.SetTrigger("Grab");
+        }
+
+
+        /*
+            Activate the looking at player 
+            on mouse right-click.
+        
         if (Input.GetMouseButtonDown(1))
         {
             LookAtPlayer();
             
         }
+        */
     }
+
+   
+   
+/*
+    Angle characters in scene to look at the player.
 
     private void LookAtPlayer()
     {
@@ -42,4 +81,5 @@ public class Chapter1 : MonoBehaviour
             character.transform.LookAt(player.transform);
         }
     }
+*/
 }
